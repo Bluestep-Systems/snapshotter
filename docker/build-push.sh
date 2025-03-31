be87d0ab0de8374ef416572d4c8a46b46dcf1f96
James Schriever <james@bluestep.net>
2025-03-31 16:10:34 -0600

    Fix path for DTZAR_HELM_KUBECTL_VERSION in README.md and build-push.sh

--- docker/build-push.sh
+++ docker/build-push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd $(dirname $0)
 set -e
-export DTZAR_HELM_KUBECTL_VERSION=$(cat ./../installers/dtzar-helm-kubectl-version.txt)
+export DTZAR_HELM_KUBECTL_VERSION=$(cat ../../installers/dtzar-helm-kubectl-version.txt)
 docker compose build b6p-snapshotter
 docker push ghcr.io/bluestep-systems/b6p-snapshotter:latest
 docker push ghcr.io/bluestep-systems/b6p-snapshotter:${DTZAR_HELM_KUBECTL_VERSION}
